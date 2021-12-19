a0567e0488bce1cbfcd1dc79dbabc4bb22b5775f
jun112465 <ktop101777@gmail.com>
2021-12-20 02:12:00 +0900

    first commit

---
+++ src/main/java/jun/studyHelper/StudyHelperApplication.java
@@ -0,0 +1,13 @@
+package jun.studyHelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jun/studyHelper/StudyHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package jun.studyHelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyHelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
