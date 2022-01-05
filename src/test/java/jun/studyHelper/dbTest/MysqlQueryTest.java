d5f11dfc78a49867da71e41282e70bd674b105a4
jun112465 <ktop101777@gmail.com>
2022-01-06 02:10:51 +0900

    update

--- src/test/java/jun/studyHelper/dbTest/MysqlQueryTest.java
+++ src/test/java/jun/studyHelper/dbTest/MysqlQueryTest.java
@@ -1,37 +1,37 @@
-package jun.studyHelper.dbTest;
-
-import jun.studyHelper.domain.member.JdbcMemberRepository;
-import jun.studyHelper.domain.member.Member;
-import jun.studyHelper.service.MemberService;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import javax.sql.DataSource;
-import java.util.Iterator;
-import java.util.List;
-
-public class MysqlQueryTest {
-
-    @Test
-    @DisplayName("testing findAll")
-    public void getAllMembers() throws ClassNotFoundException {
-        MemberService ms = new MemberService(new JdbcMemberRepository());
-        List<Member> members =  ms.findMembers();
-
-        Iterator<Member> it = members.iterator();
-        while(it.hasNext()){
-            Member m = it.next();
-            System.out.println(m.getId()+" : "+m.getName());
-        }
-    }
-
-    @Test
-    @DisplayName("testing add Member")
-    public void addMember(){
-        MemberService ms = new MemberService(new JdbcMemberRepository());
-        Member member = new Member();
-        member.setId(12345678);
-        member.setName("정정정");
-        ms.join(member);
-    }
-}
+//package jun.studyHelper.dbTest;
+//
+//import jun.studyHelper.domain.member.JdbcMemberRepository;
+//import jun.studyHelper.domain.member.Member;
+//import jun.studyHelper.service.MemberService;
+//import org.junit.jupiter.api.DisplayName;
+//import org.junit.jupiter.api.Test;
+//
+//import javax.sql.DataSource;
+//import java.util.Iterator;
+//import java.util.List;
+//
+//public class MysqlQueryTest {
+//
+//    @Test
+//    @DisplayName("testing findAll")
+//    public void getAllMembers() throws ClassNotFoundException {
+//        MemberService ms = new MemberService(new JdbcMemberRepository());
+//        List<Member> members =  ms.findMembers();
+//
+//        Iterator<Member> it = members.iterator();
+//        while(it.hasNext()){
+//            Member m = it.next();
+//            System.out.println(m.getId()+" : "+m.getName());
+//        }
+//    }
+//
+//    @Test
+//    @DisplayName("testing add Member")
+//    public void addMember(){
+//        MemberService ms = new MemberService(new JdbcMemberRepository());
+//        Member member = new Member();
+//        member.setId(12345678);
+//        member.setName("정정정");
+//        ms.join(member);
+//    }
+//}
