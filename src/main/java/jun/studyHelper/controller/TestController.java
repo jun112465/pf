75f6708a1233256425bb0573851bf25da12be834
jun112465 <ktop101777@gmail.com>
2023-03-09 12:25:29 +0900

    react integrate

---
+++ src/main/java/jun/studyHelper/controller/TestController.java
@@ -0,0 +1,12 @@
+package jun.studyHelper.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping("/api/hello")
+    public String test(){
+        return "Hello, World!";
+    }
+}
