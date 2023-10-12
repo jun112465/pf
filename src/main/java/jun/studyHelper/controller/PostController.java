4bc1bce0affccaaf328dbc72f4b7316f171c2d7d
jun112465 <ktop101777@gmail.com>
2023-10-12 11:25:01 +0900

    REFACTOR : spring mvc 패턴에 적합하도록 코드 수정

--- src/main/java/jun/studyHelper/controller/PostController.java
+++ src/main/java/jun/studyHelper/controller/PostController.java
@@ -3,6 +3,7 @@ package jun.studyHelper.controller;
 import jun.studyHelper.SessionConst;
 import jun.studyHelper.model.dto.CategoryDTO;
 import jun.studyHelper.model.dto.PostDTO;
+import jun.studyHelper.model.dto.UserDTO;
 import jun.studyHelper.model.entity.User;
 import jun.studyHelper.model.entity.Post;
 import jun.studyHelper.model.entity.Category;
@@ -12,8 +13,10 @@ import jun.studyHelper.service.PostService;
 import org.apache.commons.text.StringEscapeUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class PostController {
@@ -28,6 +31,44 @@ public class PostController {
         this.postService = postService;
         this.categoryService = categoryService;
     }
+
+
+    @GetMapping("/post/{userId}/{categoryId}")
+    public String getPosts(
+            Model model,
+            @PathVariable(name = "userId", required = false) Long userId,
+            @PathVariable(name = "categoryId", required = false) Long categoryId
+    ){
+        List<PostDTO> postDTOs;
+
+        if(userId == null){
+            postDTOs = postService.convertPostListToDTO(
+                    postService.findPostList()
+            );
+        }
+
+        else if(categoryId == null){
+            postDTOs = postService.convertPostListToDTO(
+                    postService.findUserPostList(
+                        UserDTO.builder()
+                            .id(userId)
+                            .build())
+            );
+        }
+
+        else{
+            postDTOs = postService.convertPostListToDTO(
+                    postService.getPostsByCategory(
+                    CategoryDTO.builder()
+                    .id(categoryId)
+                    .build()));
+        }
+
+        model.addAttribute("posts", postDTOs);
+
+        return "main";
+    }
+
 
     @PostMapping("notice/add-note")
     @ResponseBody
--- src/main/resources/static/js/area.js
+++ src/main/resources/static/js/area.js
@@ -1,58 +1,108 @@
 (()=>{
     let areaItems = document.getElementsByClassName("area-item")
-    let allPosts = areaItems[0];
-    let userPosts = areaItems[1];
+    let allPostBtn = areaItems[0];
+    let userPostBtn = areaItems[1];
     let allPostBox = document.getElementById("allPostList");
-    let userPostBox = document.getElementById("userPostList");
+    let userId = 1;
+    // let userPostBox = document.getElementById("userPostList");
+    let getSelectedValue = ()=>{
+        // select 태그에서 현재 선택된 옵션 요소를 가져오기
+        let select = document.getElementById("categories");
+        let selectedValue = select.options[select.selectedIndex].value;
 
-    if(userPosts.id = 'selected')
-        allPostBox.style.display = 'none';
+        // 선택된 값을 return
+        return selectedValue;
+    }
 
-    allPosts.addEventListener('click', ()=>{
+
+    // Add an event listener to the select element
+    const selectElement = document.getElementById("categories");
+    selectElement.addEventListener("change", function() {
+        // Update the content of the span element
+        let currentURL = "/";
+        // 새로운 파라미터를 추가할 수 있습니다.
+        let newParam1 = `userId=${userId}`;
+        let newParam2 = `categoryId=${getSelectedValue()}`;
+        // 현재 URL과 새로운 파라미터를 조합하여 새 URL을 생성합니다.
+        let newURL = currentURL + "?" + newParam1 + "&" + newParam2;
+        window.location.href = newURL;
+    });
+
+
+    (()=>{
+        let url = new URL(window.location.href);
+
+        // underline for btn
+        let userId = url.searchParams.get("userId");
+        if(userId == null)
+            allPostBtn.id = 'selected';
+        else
+            userPostBtn.id = 'selected';
+
+
+        // set select option with param
+        let categoryId = url.searchParams.get("categoryId")
+        if(categoryId != null){}
+        const selectElement = document.getElementById("categories");
+        if (categoryId) {
+            for (let option of selectElement.options) {
+                if (option.value === categoryId) {
+                    option.selected = true;
+                }
+            }
+        }
+
+
+    })()
+
+    allPostBtn.addEventListener('click', ()=>{
 
         // 선택된 areaItem 에 밑줄이 생기도록 하는 기능
-        allPosts.id = 'selected';
-        userPosts.id = 'unselected';
+        // allPostBtn.id = 'selected';
+        // userPostBtn.id = 'unselected';
 
-        // 선택된 areaItem 에 해당하는 postList 보여주기
-        allPostBox.style.display = "flex";
-        userPostBox.style.display = "none";
+
+        window.location.href = "/";
     })
 
-    userPosts.addEventListener('click', ()=>{
+    userPostBtn.addEventListener('click', ()=>{
 
         // 선택된 areaItem 에 밑줄이 생기도록 하는 기능
-        userPosts.id = 'selected';
-        allPosts.id = 'unselected';
+        // userPostBtn.id = 'selected';
+        // allPostBtn.id = 'unselected';
 
-        // 선택된 areaItem 에 해당하는 postList 보여주기
-        allPostBox.style.display = "none";
-        userPostBox.style.display = "flex";
+        let currentURL = "/";
+        // 새로운 파라미터를 추가할 수 있습니다.
+        let newParam1 = `userId=${userId}`;
+        let newParam2 = `categoryId=${getSelectedValue()}`;
+        // 현재 URL과 새로운 파라미터를 조합하여 새 URL을 생성합니다.
+        let newURL = currentURL + "?" + newParam1 + "&" + newParam2;
+        window.location.href = newURL;
     })
 
 
     // get categories and set selection
-    fetch('/category?' + new URLSearchParams({
-        userId : document.getElementById("userId").value
-    }))
-        .then(response => {
-            // 응답이 성공적으로 돌아오면 JSON 형식으로 파싱
-            return response.json();
-        })
-        .then(data => {
-            // JSON 데이터를 사용하여 무엇인가를 수행
-            const selectElement = document.getElementById('categories');
-            data.forEach(item => {
-                const option = document.createElement('option');
-                option.value = item.name; // 옵션의 값을 설정
-                option.text = item.name; // 옵션의 텍스트 설정
-                selectElement.appendChild(option);
-            });
-            console.log(data);
-        })
-        .catch(error => {
-            // 오류 처리
-            console.error('오류 발생:', error);
-        });
+    // fetch('/category?' + new URLSearchParams({
+    //     userId : document.getElementById("userId").value
+    // }))
+    //     .then(response => {
+    //         // 응답이 성공적으로 돌아오면 JSON 형식으로 파싱
+    //         return response.json();
+    //     })
+    //     .then(data => {
+    //         // JSON 데이터를 사용하여 무엇인가를 수행
+    //         const selectElement = document.getElementById('categories');
+    //         data.forEach(item => {
+    //             const option = document.createElement('option');
+    //             option.value = item.name; // 옵션의 값을 설정
+    //             option.text = item.name; // 옵션의 텍스트 설정
+    //             selectElement.appendChild(option);
+    //         });
+    //         console.log(data);
+    //     })
+    //     .catch(error => {
+    //         // 오류 처리
+    //         console.error('오류 발생:', error);
+    //     });
 
 })()
